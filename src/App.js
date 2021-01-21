24dde0562d7e59fd1341fe9168d5eb247e7f2bc3
thuytien1510 <thuytiencoder@gmail.com>
2021-01-21 17:29:30 +0700

    bt

--- src/App.js
+++ src/App.js
@@ -3,22 +3,47 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div className="all_">
+      <div className="profile">
+        <div className="left">
+          <div className="imga">
+            <img src="logo192.png" className="App-logo" alt="logo" />
+          </div>
+          <div className="text">
+            <h1>Hello</h1>
+            <p>My name is Thuy Tien</p>
+          </div>
+        </div>
+        <div className="right">
+          <div className="title">
+            <h1>I was born in Thua Thien Hue </h1>
+            <h1>I am 20 years old.</h1>
+          </div>
+          <div className="row">
+            <div className="column">
+              <h2>Hobby</h2>
+              <p>I am very interested in reading and writing. </p>
+            </div>
+            <div className="column">
+              <h2>Desire</h2>
+              <p>I would like to put into practice what I learned at university </p>
+            </div>
+            <div className="column">
+              <h2>Strength</h2>
+              <p>I think my strongest strength is that I am a great team player. I have ability to keep the team together.</p>
+            </div>
+
+            <div className="column">
+              <h2>Weakness</h2>
+              <p>My English ability can be my biggest problem at the moment. But, that is only a temporary problem. I will try my best to improve my English as fast as possible.</p>
+            </div>
+          </div>
+
+        </div>
+      </div> 
+      </div> 
+    
   );
 }
 
